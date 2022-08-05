86fa610629ddc87dae9fa9c4ce2509215b8070dd
Morten Aslak <moaslak@gmail.com>
2022-08-05 12:08:53 +0200

    mySqlConnecter added

---
+++ WpfPlay/Database/MySQLConnecter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WpfPlay.Database
+{
+    public class MySQLConnecter
+    {
+        private MySQLConnecter()
+        {
+
+        }
+        public string server { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string databaseName { get; set; }
+        private MySqlConnection Connection { get; set; }        
+        private static MySQLConnecter instance = null;
+        public static MySQLConnecter Instance()
+        {
+            if(instance == null)
+                instance = new MySQLConnecter();
+            return instance;
+        }
+
+        public bool Connected()
+        {
+            if(Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connectionString = string.Format("Server={0}; database={1}; UID={2}; password={3}", server, databaseName, username, password);
+                Connection = new MySqlConnection(connectionString);
+                Connection.Open();
+            }
+            return true;
+        }
+
+        public void CloseConnection()
+        {
+            Connection.Close();
+        }
+    }
+}
--- WpfPlay/MainWindow.xaml.cs
+++ WpfPlay/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace WpfPlay
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<string> list = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (!(list.Contains(textbox.Text)) && textbox.Text != "")
+            {
+                list.Add(textbox.Text);
+                listbox.Items.Clear();
+
+                foreach (string item in list)
+                    listbox.Items.Add(item);
+
+                textbox.Text = "";
+            }
+            
+        }
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            if (listbox.SelectedItem != null)
+            {
+                int index = listbox.SelectedIndex;
+                list.RemoveAt(index);
+            }
+
+            listbox.Items.Clear();
+
+            foreach (string item in list)
+                listbox.Items.Add(item);
+        }
+
+        private void listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void WinnerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Winner(object sender, RoutedEventArgs e)
+        {
+            if (list.Count == 0)
+                return;
+
+            Random rand = new Random();
+            int winner = rand.Next(0, list.Count);
+
+            MessageBox.Show("The winner is: " + list[winner]);
         }
     }
 }
